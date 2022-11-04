db9aaec230f8e3f4e5c873492c3b2a02c3b7dd3c
Darya <HavrylenkoIT@gmail.com>
2022-11-04 23:13:13 +0200

    add styles

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,11 +1,25 @@
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import styled from 'styled-components';
+
+const List = styled.ul`
+     display: grid;
+  max-width: calc(100vw - 48px);
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-gap: 16px;
+  margin-top: 0;
+  margin-bottom: 0;
+  padding: 0;
+  list-style: none;
+  margin-left: auto;
+  margin-right: auto;
+`
 
 export const ImageGallery = ({ items}) => {
     return (
-        <ul>
+        <List>
             {items.map(({ id, webformatURL, largeImageURL }) => {
                 return <ImageGalleryItem key={id} webformatURL={webformatURL} largeImageURL={largeImageURL}></ImageGalleryItem>
   })}
-</ul>
+</List>
     )
 }
--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,7 +1,25 @@
-export const ImageGalleryItem = ({webformatURL, largeImageURL}) => {
+import styled from 'styled-components';
+
+const Item = styled.li`
+  border-radius: 2px;
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);  
+`;
+const Image = styled.img`
+   width: 100%;
+  height: 260px;
+  object-fit: cover;
+  transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);  
+  &:hover{
+    transform: scale(1.03);
+  cursor: zoom-in;
+  } 
+`
+
+export const ImageGalleryItem = ({ webformatURL, largeImageURL }) => {
     return (
-    <li><a href={largeImageURL}> <img src={webformatURL} alt="" /></a>
+    <Item><a href={largeImageURL}> <Image src={webformatURL} alt="" /></a>
  
-</li>)
+</Item>)
 
 }
